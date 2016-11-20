f3fab1d40e4d9f0b9a0630111eef0e71196d7921
Aleksej Filippovich <alex.filippovich@gmail.com>
2016-11-20 19:57:59 +0200

    Update web-font-loader.js
    fix js-error

--- web-font-loader.js
+++ web-font-loader.js
@@ -169,7 +169,7 @@ function loadFonts(fontsArray, options) {
 		}
 
 		var f = new FontFace('t', 'url( "data:application/font-woff2;base64,d09GMgABAAAAAADcAAoAAAAAAggAAACWAAEAAAAAAAAAAAAAAAAAAAAAAAAAAAAABk4ALAoUNAE2AiQDCAsGAAQgBSAHIBtvAcieB3aD8wURQ+TZazbRE9HvF5vde4KCYGhiCgq/NKPF0i6UIsZynbP+Xi9Ng+XLbNlmNz/xIBBqq61FIQRJhC/+QA/08PJQJ3sK5TZFMlWzC/iK5GUN40psgqvxwBjBOg6JUSJ7ewyKE2AAaXZrfUB4v+hze37ugJ9d+DeYqiDwVgCawviwVFGnuttkLqIMGivmDg" ) format( "woff2" )', {});
-		f.load().catch.(function () {
+		f.load()['catch'](function () {
 		});
 
 		return f.status == 'loading' || f.status == 'loaded';
